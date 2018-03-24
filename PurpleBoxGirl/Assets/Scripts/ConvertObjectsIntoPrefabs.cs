e16178a75b5d454cebebb25326a0558cf94ea512
superjug <superjug@gmail.com>
2018-03-24 02:28:48 +0000

    Wrote Script to convert objects into prefabs & started working on shadows

---
+++ PurpleBoxGirl/Assets/Scripts/ConvertObjectsIntoPrefabs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConvertObjectsIntoPrefabs : MonoBehaviour {
+	public Transform[] objectsToConvert;
+	public GameObject thingToReplaceWith;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			foreach (Transform nestedPrefab in objectsToConvert) {
+				if (child.name == nestedPrefab.name) {
+					Vector3 childPos = child.position;
+					Quaternion childRot = child.rotation;
+
+					Destroy(child.gameObject);
+					Instantiate(thingToReplaceWith, childPos, childRot, transform);
+				}
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PurpleBoxGirl/Assets/Scripts/SpriteShadow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadow : MonoBehaviour {
+	//public Camera camera;
+	public Vector2 shadowOffset = new Vector2(-0.3f, -0.1f);
+
+	private SpriteRenderer sprRndCaster;
+	private SpriteRenderer sprRndShadow;
+
+	private Transform transformCaster;
+	private Transform transformShadow;
+
+	public Material shadowMaterial;
+	public Color shadowColor;
+
+	// Use this for initialization
+	void Start () {
+		//camera = FindObjectOfType<Camera>();
+		transformCaster = transform;
+		transformShadow = new GameObject ().transform;
+		transformShadow.parent = transformCaster;
+		transformShadow.gameObject.name = "Shadow";
+		transformShadow.localRotation = Quaternion.identity;
+
+		sprRndCaster = GetComponent<SpriteRenderer> ();
+		sprRndShadow = transformShadow.gameObject.AddComponent<SpriteRenderer> ();
+
+		sprRndShadow.material = shadowMaterial;
+		sprRndShadow.color = shadowColor;
+		sprRndShadow.sortingLayerName = "Background";
+		sprRndShadow.sortingOrder = 1;
+	}
+
+	void LateUpdate () {
+		/*
+		Vector3 cameraPos = camera.transform.position;
+		cameraPos.x = cameraPos.x;
+
+		float distance = transformCaster.position.x - cameraPos.x;
+
+		//shadowOffset.x = Vector3.MoveTowards(transformCaster.position, cameraPos, distance/3f).x;
+		shadowOffset.x = (transformCaster.position.x - cameraPos.x)/10f;
+		shadowOffset.y = (transformCaster.position.y - cameraPos.y)/10f;
+		*/
+		transformShadow.position = new Vector2 (transformCaster.position.x + shadowOffset.x, transformCaster.position.y + shadowOffset.y);
+		transformShadow.localScale = new Vector3(1.2f,1.2f,0f);
+		sprRndShadow.sprite = sprRndCaster.sprite;
+	}
+}
