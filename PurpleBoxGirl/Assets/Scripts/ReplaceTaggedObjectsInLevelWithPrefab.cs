8f21b61c14bea91c7244db13b5951a379ca434b3
superjug <superjug@gmail.com>
2018-03-24 17:35:06 +0000

    Greatly improved efficiency of replacing objects in levels with prefabs & increased efficiency of shadows for non-moving objects

---
+++ PurpleBoxGirl/Assets/Scripts/ReplaceTaggedObjectsInLevelWithPrefab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceTaggedObjectsInLevelWithPrefab : MonoBehaviour {
+	public string[] tagsInOrder;
+	public GameObject[] prefabsInOrder;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			Vector3 childPos = child.position;
+			Quaternion childRot = child.rotation;
+
+			for (int i = 0; i < tagsInOrder.Length; i++)
+			{
+				if(child.CompareTag(tagsInOrder[i])){
+					Destroy (child.gameObject);
+					Instantiate (prefabsInOrder[i], childPos, childRot, transform);
+					break;
+				}
+			}
+
+		}
+
+	}
+
+}
--- PurpleBoxGirl/Assets/Scripts/SpriteShadow.cs
+++ PurpleBoxGirl/Assets/Scripts/SpriteShadow.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteShadow : MonoBehaviour {
-	//public Camera camera;
 	public Vector2 shadowOffset = new Vector2(-0.3f, -0.1f);
 
 	private SpriteRenderer sprRndCaster;
@@ -15,11 +14,12 @@ public class SpriteShadow : MonoBehaviour {
 	public Material shadowMaterial;
 	public Color shadowColor;
 
+	public bool updatePosition = true;
+
 	// Use this for initialization
 	void Start () {
-		//camera = FindObjectOfType<Camera>();
 		transformCaster = transform;
-		transformShadow = new GameObject ().transform;
+		transformShadow = new GameObject().transform;
 		transformShadow.parent = transformCaster;
 		transformShadow.gameObject.name = "Shadow";
 		transformShadow.localRotation = Quaternion.identity;
@@ -31,21 +31,16 @@ public class SpriteShadow : MonoBehaviour {
 		sprRndShadow.color = shadowColor;
 		sprRndShadow.sortingLayerName = "Background";
 		sprRndShadow.sortingOrder = 1;
+
+		transformShadow.position = new Vector2 (transformCaster.position.x + shadowOffset.x, transformCaster.position.y + shadowOffset.y);
+		sprRndShadow.sprite = sprRndCaster.sprite;
+		transformShadow.localScale = new Vector3(1.2f,1.1f,0f);
 	}
 
 	void LateUpdate () {
-		/*
-		Vector3 cameraPos = camera.transform.position;
-		cameraPos.x = cameraPos.x;
-
-		float distance = transformCaster.position.x - cameraPos.x;
-
-		//shadowOffset.x = Vector3.MoveTowards(transformCaster.position, cameraPos, distance/3f).x;
-		shadowOffset.x = (transformCaster.position.x - cameraPos.x)/10f;
-		shadowOffset.y = (transformCaster.position.y - cameraPos.y)/10f;
-		*/
-		transformShadow.position = new Vector2 (transformCaster.position.x + shadowOffset.x, transformCaster.position.y + shadowOffset.y);
-		transformShadow.localScale = new Vector3(1f,1.1f,0f);
-		sprRndShadow.sprite = sprRndCaster.sprite;
+		if (updatePosition) {
+			transformShadow.position = new Vector2 (transformCaster.position.x + shadowOffset.x, transformCaster.position.y + shadowOffset.y);
+			sprRndShadow.sprite = sprRndCaster.sprite;
+		}
 	}
 }
